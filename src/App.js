4511120e15934f3a2c47d550134b7091c1219edb
Yusuf Arslan <yusufarsln98@outlook.com>
2023-02-22 12:41:00 +0300

    images added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React from 'react';
+import { ReactPhotoSphereViewer, EquirectangularTilesAdapter } from "react-photo-sphere-viewer";
+
 
 function App() {
-  return (
-    <div>
+  const pSRef = React.useRef(null);
+  const baseUrl = './assets/';
+  // const baseUrl = "https://photo-sphere-viewer-data.netlify.app/assets/";
 
+  return (
+    <div style={{ width: '100%', height: '100%', top: '0', left: '0', position: 'absolute' }}>
+      <ReactPhotoSphereViewer
+        height={"100vh"}
+        width={"100%"}
+        keyboard={false}
+        adapter={EquirectangularTilesAdapter}
+        src={{
+          width: 14400,
+          cols: 16,
+          rows: 8,
+          baseUrl: `${baseUrl}pano.jpg`,
+          tileUrl: (col, row) => {
+            return `${baseUrl}tiles/row-${row}-column-${col}.jpg`;
+          }
+        }}
+      />
     </div>
   );
 }
